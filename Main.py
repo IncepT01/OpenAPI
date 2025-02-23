8a21932b7353dde169d56f6f7eeacaadf2963cb8
IncepT01 <tiszagergo10@gmail.com>
2025-02-23 17:53:16 +0100

    Comments

--- Main.py
+++ Main.py
@@ -6,19 +6,23 @@ from data import API_KEY
 def GenerateResponse(message, personality=""):
     url = 'https://api.openai.com/v1/chat/completions'
 
+    #Header with the API auth and to specify the json format
     headers = {
         "Authorization": "Bearer " + API_KEY,
         "Content-Type": "application/json"
     }
 
+    #Payload we send
     data = {
         "model": "gpt-3.5-turbo",
         "messages" : [{"role": "system", "content": personality},{"role": "user", "content": message_to_ask}],
         "temperature": 0.7
     }
 
+    #Post request
     resp = requests.post(url=url, json = data, headers=headers)
 
+    #Error handling and response file generation
     if resp.status_code == 200:
         print("Success!")
         resp_data = resp.json()
@@ -31,18 +35,22 @@ def GenerateResponse(message, personality=""):
 
 
 
-
+#message to prompt
 message_to_ask = "I'm trying out the new openAPI. If you get this message, please welcome the audience"
 
+#Personality for chatGPT
 personality = '''You are a sports commentator who is currently going through a divorce.
                 Everything around you reminds you of your ex-wife, and you occasionally bring up memoried
                 "f her while commenting on sports events. Despite this, you try to keep your commentary focused
                 "n the game, but emotions sometimes creep in.'''
 
-GenerateResponse(message_to_ask, personality)
+#Send the API post request and create a json file fromm the response
+#GenerateResponse(message_to_ask, personality)
 
+#Load the response json
 with open("chat_response.json", 'r') as f:
     resp_data = json.load(f)
 
+#print the response
 print("ChatGPT says:")
 print(resp_data['choices'][0]['message']['content'])
