a6c0b6d21cd24af2a968a4e9139bd401dd6e212f
IncepT01 <tiszagergo10@gmail.com>
2025-02-23 15:46:11 +0100

    Seperate Json

--- Main.py
+++ Main.py
@@ -2,30 +2,42 @@ import requests
 import json
 from data import API_KEY
 
-url = 'https://api.openai.com/v1/chat/completions'
 
-message_to_ask = "I am testing the openAPI using python. If you received this message. Say something to welcome the audience"
+def GenerateResponse(message):
+    url = 'https://api.openai.com/v1/chat/completions'
 
-headers = {
-    "Authorization": "Bearer " + API_KEY,
-    "Content-Type": "application/json"
-}
+    headers = {
+        "Authorization": "Bearer " + API_KEY,
+        "Content-Type": "application/json"
+    }
 
-data = {
-    "model": "gpt-3.5-turbo",
-    "messages" : [{"role": "user", "content": message_to_ask}],
-    "temperature": 0.7
-}
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages" : [{"role": "user", "content": message_to_ask}],
+        "temperature": 0.7
+    }
 
-resp = requests.post(url=url, json = data, headers=headers)
+    resp = requests.post(url=url, json = data, headers=headers)
 
-if resp.status_code == 200:
-    resp_data = resp.json()
+    if resp.status_code == 200:
+        print("Success!")
+        resp_data = resp.json()
 
-    with open('chat_response.json', 'w') as f:
-        json.dump(resp_data, f, indent=4)
+        with open('chat_response.json', 'w') as f:
+            json.dump(resp_data, f, indent=4)
+    else:
+        print("Communication error")
 
-    messsage = resp_data['choices'][0]['content']
-    print(messsage)
-else:
-    print("Communication error")
+
+
+
+
+message_to_ask = "Hey there chatGPT! I'm talking to you through openAPI. Did you get the message?"
+
+#GenerateResponse(message_to_ask)
+
+with open("chat_response.json", 'r') as f:
+    resp_data = json.load(f)
+
+print("ChatGPT says:")
+print(resp_data['choices'][0]['message']['content'])
